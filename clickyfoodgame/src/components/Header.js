e410d56e417b38b46c9b09f343c5ff1c1f59ffca
Yash Arya <yasharya11@gmail.com>
2018-05-12 15:07:08 -0700

    mvp

--- clickyfoodgame/src/components/Header.js
+++ clickyfoodgame/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = (props)=> (
-    <nav style={{background: "tan", color: "brown"}} className={"navbar navbar-default sticky-top"}>
+    <nav style={{background: "tan", color: "brown", align: "center"}} className={"navbar navbar-default sticky-top"}>
         <div className={"conatiner"}>
             <div><h1>Food Clicker Game</h1></div>
             <div>CURRENT SCORE:{props.score}</div>
